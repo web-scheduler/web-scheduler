589ce91b99bab5335981052ce5fc2c5468b8b36d
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-01-26 17:21:05 -0500

    add created and modified fields to ScheduledTaskMetadata (#21)

--- Source/WebScheduler.Abstractions/Grains/Scheduler/IScheduledTaskGrain.cs
+++ Source/WebScheduler.Abstractions/Grains/Scheduler/IScheduledTaskGrain.cs
@@ -28,23 +28,29 @@ public interface IScheduledTaskGrain : IGrainWithStringKey
 /// </summary>
 public class ScheduledTaskMetadata
 {
+
+    /// <summary>
+    /// Created timestamp.
+    /// </summary>
+    public DateTimeOffset Created { get; set; }
+
+    /// <summary>
+    /// Last modified timestamp.
+    /// </summary>
+    public DateTimeOffset Modified { get; set; }
+
     /// <summary>
     /// The name of the scheduled task.
     /// </summary>
-    [Required]
-    [Display(Name = "Task Name", Description = "The name of the task.", ShortName = "Name")]
     public string Name { get; set; } = string.Empty;
 
     /// <summary>
     /// The description of the scheduled task.
     /// </summary>
-    [Required]
-    [Display(Name = "Task Description", Description = "The description of what the task does.", ShortName = "Description")]
     public string Description { get; set; } = string.Empty;
 
     /// <summary>
     /// Specifies if the task is enabled.
     /// </summary>
-    [Display(Name = "Enabled", Description = "Determines if the task is schedulable.", ShortName = "Enabled")]
     public bool IsEnabled { get; set; }
 }
