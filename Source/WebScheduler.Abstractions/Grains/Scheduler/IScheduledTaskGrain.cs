98667282e2d44e191c2f8fcede3129d376a7cdb0
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-01-29 14:13:34 -0500

    Allow config of relative path for orleans dashboard, default to /OrleansDashboard

--- Source/WebScheduler.Abstractions/Grains/Scheduler/IScheduledTaskGrain.cs
+++ Source/WebScheduler.Abstractions/Grains/Scheduler/IScheduledTaskGrain.cs
@@ -22,35 +22,3 @@ public interface IScheduledTaskGrain : IGrainWithStringKey
     /// <returns>The scheduled task metadata.</returns>
     ValueTask<ScheduledTaskMetadata?> GetAsync();
 }
-
-/// <summary>
-/// Model for Scheduled Task metadata
-/// </summary>
-public class ScheduledTaskMetadata
-{
-
-    /// <summary>
-    /// Created timestamp.
-    /// </summary>
-    public DateTimeOffset Created { get; set; }
-
-    /// <summary>
-    /// Last modified timestamp.
-    /// </summary>
-    public DateTimeOffset Modified { get; set; }
-
-    /// <summary>
-    /// The name of the scheduled task.
-    /// </summary>
-    public string Name { get; set; } = string.Empty;
-
-    /// <summary>
-    /// The description of the scheduled task.
-    /// </summary>
-    public string Description { get; set; } = string.Empty;
-
-    /// <summary>
-    /// Specifies if the task is enabled.
-    /// </summary>
-    public bool IsEnabled { get; set; }
-}
