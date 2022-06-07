c214489f4657df6849f6ccb20ed7a95fbdcd5e10
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-06-07 01:09:43 -0400

    Add Header support to Http Trigger (#130)

--- Source/WebScheduler.Abstractions/Grains/Scheduler/HttpTriggerProperties.cs
+++ Source/WebScheduler.Abstractions/Grains/Scheduler/HttpTriggerProperties.cs
@@ -26,4 +26,35 @@ public class HttpTriggerProperties
     [Required]
     [Display(Name = "HTTP Method", Description = "The HTTP method to use for the request.")]
     public string HttpMethod { get; set; } = System.Net.Http.HttpMethod.Get.Method;
+
+    /// <summary>
+    /// The Request Headers to send with the Http request.
+    /// </summary>
+    [Display(Name = "Headers", Description = "The Request Headers to use for the request.")]
+    public List<Header> Headers { get; set; } = new();
+}
+
+/// <summary>
+/// Represents a Header Key Value Pair.
+/// </summary>
+public class Header
+{
+    /// <summary>
+    /// Default constructor.
+    /// </summary>
+    public Header()
+    { }
+
+    /// <summary>
+    /// The Key name.
+    /// </summary>
+    [Required]
+    [Display(Name = "Key", Description = "The header key.")]
+    public string Key { get; set; } = string.Empty;
+
+    /// <summary>
+    /// The Value.
+    /// </summary>
+    [Display(Name = "Key", Description = "The header value.")]
+    public string Value { get; set; } = string.Empty;
 }
