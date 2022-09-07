d9303c1f8e4360e08aaa82b5962e9d7abe8aab6a
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-09-06 20:54:59 -0400

    Update query so it preserves table annotations

--- Source/WebScheduler.DataMigrations/Migrations/20220902235510_Add IX_GrainIdHash_GrainTypeHash to OrleansStorage table.cs
+++ Source/WebScheduler.DataMigrations/Migrations/20220902235510_Add IX_GrainIdHash_GrainTypeHash to OrleansStorage table.cs
@@ -6,13 +6,27 @@ namespace WebScheduler.DataMigrations.Migrations
 {
     public partial class AddIX_OrleansStoragetoOrleansStoragetable : Migration
     {
-        protected override void Up(MigrationBuilder migrationBuilder) => migrationBuilder.CreateIndex(
-                name: "IX_OrleansStorage",
-                table: "OrleansStorage",
-                columns: new[] { "GrainIdHash", "GrainTypeHash" });
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("/*!80013 SET SQL_REQUIRE_PRIMARY_KEY = 0 */;");
 
-        protected override void Down(MigrationBuilder migrationBuilder) => migrationBuilder.DropIndex(
+            migrationBuilder.Sql("ALTER TABLE `OrleansStorage` DROP PRIMARY KEY, ADD PRIMARY KEY(Id);");
+
+            migrationBuilder.Sql("/*!80013 SET SQL_REQUIRE_PRIMARY_KEY = 1 */;");
+            migrationBuilder.Sql("ALTER TABLE `OrleansStorage` ADD INDEX IX_OrleansStorage (GrainIdHash, GrainTypeHash);");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("/*!80013 SET SQL_REQUIRE_PRIMARY_KEY = 0 */;");
+
+            migrationBuilder.DropIndex(
                 name: "IX_OrleansStorage",
                 table: "OrleansStorage");
+
+            migrationBuilder.DropPrimaryKey("PRIMARY", "OrleansStorage");
+            migrationBuilder.Sql("ALTER TABLE `OrleansStorage` ADD PRIMARY KEY(Id, GrainIdHash, GrainTypeHash);");
+            migrationBuilder.Sql("/*!80013 SET SQL_REQUIRE_PRIMARY_KEY = 1 */;");
+        }
     }
 }
