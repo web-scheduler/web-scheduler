3f5fb5e6aeeea270750090830c5c483ac1875dd8
Elan Hasson <234704+ElanHasson@users.noreply.github.com>
2022-09-16 16:20:17 -0400

    Migrate from Guid-based TenantId to string-based TenantIdAsString (#269)
    * Migrate from Guid-based TenantId to string-based TenantIdAsString

--- Source/WebScheduler.Abstractions/Grains/ITenantScopedGrain.cs
+++ Source/WebScheduler.Abstractions/Grains/ITenantScopedGrain.cs
@@ -9,14 +9,22 @@ using Orleans.CodeGeneration;
 /// Used to validate tenant access.
 /// </summary>
 /// <typeparam name="TGrainInterface"></typeparam>
-[Version(1)]
+[Version(2)]
 public interface ITenantScopedGrain<TGrainInterface> : IGrainWithStringKey
     where TGrainInterface : IGrain
 {
     /// <summary>
     /// Checks if a resource is owned by a tenantId.
     /// </summary>
+    /// <param name="tenantId">The tenant id</param>
+    /// <returns>True if owned, false if not, null if unset.</returns>
+    ValueTask<bool?> IsOwnedByAsync(string tenantId);
+
+    /// <summary>
+    /// Checks if a resource is owned by a tenantId.
+    /// </summary>
     /// <param name="tenantId">The tenat id</param>
     /// <returns>True if owned, false if not, null if unset.</returns>
+    [Obsolete("Use IsOwnedByAsync(string tenantId) instead.", false, UrlFormat = "https://github.com/web-scheduler/web-scheduler/issues/268")]
     ValueTask<bool?> IsOwnedByAsync(Guid tenantId);
 }
